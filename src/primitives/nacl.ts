d756f46074061f12693f105cf780f4b5bc91fda7
Steve Korshakov <steve@korshakov.com>
2021-09-13 18:02:34 +0300

    feat: eexport keyPairFromSeed

--- src/primitives/nacl.ts
+++ src/primitives/nacl.ts
@@ -14,6 +14,15 @@ export function keyPairFromSecretKey(secretKey: Buffer): KeyPair {
     }
 }
 
+export function keyPairFromSeed(secretKey: Buffer): KeyPair {
+    let res = nacl.sign.keyPair.fromSeed(new Uint8Array(secretKey));
+
+    return {
+        publicKey: Buffer.from(res.publicKey),
+        secretKey: Buffer.from(res.secretKey),
+    }
+}
+
 export function sign(data: Buffer, secretKey: Buffer) {
     return Buffer.from(nacl.sign.detached(new Uint8Array(data), new Uint8Array(secretKey)));
 }
