2f4e12e5102caff257eb2d2347954b6d94c2f58f
Steve Korshakov <steve@korshakov.com>
2021-09-15 18:13:54 +0300

    fix: fix pbkdf2 on react native

--- src/primitives/pbkdf2_sha512.ts
+++ src/primitives/pbkdf2_sha512.ts
@@ -19,8 +19,8 @@ export async function pbkdf2_sha512(key: string | Buffer, salt: string | Buffer,
     } else {
         const keyBuffer = typeof key === 'string' ? Buffer.from(key, 'utf-8') : key;
         const saltBuffer = typeof salt === 'string' ? Buffer.from(salt, 'utf-8') : salt;
-        let pbkdf2 = require('react-native-fast-pbkdf2');
-        let res = await pbkdf2.derive(keyBuffer.toString('base64'), saltBuffer.toString('base64'), 1, 16, 'sha-512');
+        let pbkdf2 = require('react-native-fast-pbkdf2').default;
+        let res = await pbkdf2.derive(keyBuffer.toString('base64'), saltBuffer.toString('base64'), iterations, keyLen, 'sha-512');
         return Buffer.from(res, 'base64');
     }
 }
