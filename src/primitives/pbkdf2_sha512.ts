a11a38621e292aefe1d12abc595008a9011ad97a
Steve Korshakov <steve@korshakov.com>
2021-09-15 18:07:36 +0300

    wip: working on pbkdf2 integration

--- src/primitives/pbkdf2_sha512.ts
+++ src/primitives/pbkdf2_sha512.ts
@@ -17,6 +17,10 @@ export async function pbkdf2_sha512(key: string | Buffer, salt: string | Buffer,
         const derivedBits = await window.crypto.subtle.deriveBits({ name: "PBKDF2", hash: "SHA-512", salt: saltBuffer, iterations: iterations }, pbkdf2_key, keyLen * 8);
         return Buffer.from(derivedBits);
     } else {
-        throw Error('Unsupported');
+        const keyBuffer = typeof key === 'string' ? Buffer.from(key, 'utf-8') : key;
+        const saltBuffer = typeof salt === 'string' ? Buffer.from(salt, 'utf-8') : salt;
+        let pbkdf2 = require('react-native-fast-pbkdf2');
+        let res = await pbkdf2.derive(keyBuffer.toString('base64'), saltBuffer.toString('base64'), 1, 16, 'sha-512');
+        return Buffer.from(res, 'base64');
     }
 }
