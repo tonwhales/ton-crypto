8795c7936b235a233705b3272f49633f5df5f61b
Steve Korshakov <steve@korshakov.com>
2022-01-04 23:28:57 +0300

    feat: webpack support

--- src/primitives/pbkdf2_sha512.native.ts
+++ src/primitives/pbkdf2_sha512.native.ts
@@ -17,6 +17,10 @@ export async function pbkdf2_sha512(key: string | Buffer, salt: string | Buffer,
         const derivedBits = await window.crypto.subtle.deriveBits({ name: "PBKDF2", hash: "SHA-512", salt: saltBuffer, iterations: iterations }, pbkdf2_key, keyLen * 8);
         return Buffer.from(derivedBits);
     } else {
-        throw Error('pbkdf2 is unsupported');
+        const keyBuffer = typeof key === 'string' ? Buffer.from(key, 'utf-8') : key;
+        const saltBuffer = typeof salt === 'string' ? Buffer.from(salt, 'utf-8') : salt;
+        let pbkdf2 = require('react-native-fast-pbkdf2').default;
+        let res = await pbkdf2.derive(keyBuffer.toString('base64'), saltBuffer.toString('base64'), iterations, keyLen, 'sha-512');
+        return Buffer.from(res, 'base64');
     }
 }
